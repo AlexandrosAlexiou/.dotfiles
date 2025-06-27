b3cdb76cb1b088dde37165f0adfba3dff68c8eaa
Alexandros Alexiou <alex.alexiouu@gmail.com>
2025-06-27 17:11:40 +0300

    Add specific JRE for kotlin-lsp

--- nvim/lua/tt/plugins.lua
+++ nvim/lua/tt/plugins.lua
@@ -897,8 +897,7 @@ return {
                     "mvnw",
                     "settings.gradle",
                 },
-                -- Optional: Specify a custom Java path
-                -- jre_path = "/path/to/jdk"
+                jre_path = os.getenv "JDK21",
             }
         end,
     },
