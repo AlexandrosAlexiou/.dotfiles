1f9f137caaea44e70fe83110a73cf5c57a62ad92
Alexandros Alexiou <alex.alexiouu@gmail.com>
2025-08-09 13:09:46 +0300

    Add `yazi.nvim`

--- nvim/lua/tt/plugins.lua
+++ nvim/lua/tt/plugins.lua
@@ -901,4 +901,44 @@ return {
             }
         end,
     },
+
+    -- Plugin for the yazi terminal file manager
+    {
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", lazy = true },
+        },
+        keys = {
+            {
+                "<leader>yc",
+                mode = { "n", "v" },
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                "<c-up>",
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        opts = {
+            open_for_directories = false,
+            keymaps = {
+                show_help = "<f1>",
+            },
+        },
+        init = function()
+            -- mark netrw as loaded so it's not loaded at all.
+            --
+            -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+            vim.g.loaded_netrwPlugin = 1
+        end,
+    },
 }
