012bf0d3b9e9b210e30ba1eb008cde0b62b582d9
Alexandros Alexiou <alex.alexiouu@gmail.com>
2026-08-11 21:53:48 +0300

    Fix explorer responsiveness with splits

--- nvim/lua/tt/_plugins/snacks/explorer.lua
+++ nvim/lua/tt/_plugins/snacks/explorer.lua
@@ -69,6 +69,17 @@ M.explorer = {
         root:on("WinResized", function()
             update(preview_win)
         end)
+
+        -- Trigger split responsiveness when explorer opens
+        vim.schedule(function()
+            vim.cmd "wincmd ="
+        end)
+    end,
+    on_hide = function(picker)
+        -- Trigger split responsiveness when explorer closes
+        vim.schedule(function()
+            vim.cmd "wincmd ="
+        end)
     end,
     actions = {
         toggle_preview = function(picker)
