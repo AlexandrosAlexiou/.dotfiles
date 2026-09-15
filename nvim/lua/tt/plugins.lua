9937881c923efa2eb35ea5f118aad5c6821492ef
Alexandros Alexiou <alex.alexiouu@gmail.com>
2026-09-15 21:39:14 +0300

    Add all lsp's related to Java and Kotlin disabled

--- nvim/lua/tt/plugins.lua
+++ nvim/lua/tt/plugins.lua
@@ -659,6 +659,17 @@ return {
         end,
     },
 
+    -- Java development utilities
+    {
+        "mfussenegger/nvim-jdtls",
+        ft = { "java" },
+        dependencies = { "neovim/nvim-lspconfig" },
+        config = function()
+            require("tt._plugins.jdtls").setup()
+        end,
+        enabled = false,
+    },
+
     -- IntelliJ LSP server
     {
         dir = "~/Sources/intellij-server.nvim",
@@ -712,6 +723,7 @@ return {
                 dap = { enabled = true },
             }
         end,
+        enabled = true,
     },
 
     -- Rust development utilities
@@ -725,41 +737,42 @@ return {
         end,
     },
 
-    -- -- Kotlin development utilities
-    -- {
-    --     "AlexandrosAlexiou/kotlin.nvim",
-    --     ft = { "kotlin" },
-    --     dependencies = { "mason.nvim", "mason-lspconfig.nvim", "oil.nvim" },
-    --     config = function()
-    --         require("kotlin").setup {
-    --             root_markers = {
-    --                 "gradlew",
-    --                 ".git",
-    --                 "mvnw",
-    --                 "settings.gradle",
-    --             },
-    --             jvm_args = {
-    --                 "-Xmx4G",
-    --                 "-Xms4G",
-    --             },
-    --             inlay_hints = {
-    --                 enabled = true, -- Enable inlay hints (auto-enable on LSP attach)
-    --                 parameters = true, -- Show parameter names
-    --                 parameters_compiled = true, -- Show compiled parameter names
-    --                 parameters_excluded = false, -- Show excluded parameter names
-    --                 types_property = true, -- Show property types
-    --                 types_variable = true, -- Show local variable types
-    --                 function_return = true, -- Show function return types
-    --                 function_parameter = true, -- Show function parameter types
-    --                 lambda_return = true, -- Show lambda return types
-    --                 lambda_receivers_parameters = true, -- Show lambda receivers/parameters
-    --                 value_ranges = true, -- Show value ranges
-    --                 kotlin_time = true, -- Show kotlin.time warnings
-    --                 call_chains = true, -- Show call-chain intermediate types
-    --             },
-    --         }
-    --     end,
-    -- },
+    -- Kotlin development utilities
+    {
+        dir = "~/Sources/kotlin.nvim",
+        ft = { "kotlin" },
+        dependencies = { "mason.nvim", "mason-lspconfig.nvim", "oil.nvim" },
+        config = function()
+            require("kotlin").setup {
+                root_markers = {
+                    "gradlew",
+                    ".git",
+                    "mvnw",
+                    "settings.gradle",
+                },
+                jvm_args = {
+                    "-Xmx4G",
+                    "-Xms4G",
+                },
+                inlay_hints = {
+                    enabled = true, -- Enable inlay hints (auto-enable on LSP attach)
+                    parameters = true, -- Show parameter names
+                    parameters_compiled = true, -- Show compiled parameter names
+                    parameters_excluded = false, -- Show excluded parameter names
+                    types_property = true, -- Show property types
+                    types_variable = true, -- Show local variable types
+                    function_return = true, -- Show function return types
+                    function_parameter = true, -- Show function parameter types
+                    lambda_return = true, -- Show lambda return types
+                    lambda_receivers_parameters = true, -- Show lambda receivers/parameters
+                    value_ranges = true, -- Show value ranges
+                    kotlin_time = true, -- Show kotlin.time warnings
+                    call_chains = true, -- Show call-chain intermediate types
+                },
+            }
+        end,
+        enabled = false,
+    },
 
     -- Full-text search powered by Tantivy
     {
